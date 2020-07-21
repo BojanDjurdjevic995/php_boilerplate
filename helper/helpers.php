17913cf3570fe7edbbf402ca7c392b9a19301bc0
bojan.djurdjevic <djurdjevicbojan12@gmail.com>
2020-07-21 08:22:08 +0200

    composer and helpers changes

--- helper/helpers.php
+++ helper/helpers.php
@@ -2,6 +2,7 @@
     use App\Controllers\Request;
     use App\Controllers\Redirect;
     use App\Controllers\Session;
+    use Illuminate\Support\Str;
 
     function timeStamp()
     {
@@ -55,5 +56,25 @@
                 return Session::get();
             if ($destroy)
                 return Session::destroy($destroy);
+        }
+    }
+
+    if (!function_exists('csrf_token')) {
+        function csrf_token() {
+            if (!session('_token'))
+                session('_token', Str::random(40));
+            return session('_token');
+        }
+    }
+
+    if (!function_exists('csrf_field')) {
+        function csrf_field() {
+            return '<input type="hidden" name="csrf_token" value="'.csrf_token().'">';
+        }
+    }
+
+    if (!function_exists('csrf_meta')) {
+        function csrf_meta() {
+            return '<meta name="csrf-token" content="'.csrf_token().'">';
         }
     }
--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,8 @@ $page = end($page);
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <?= csrf_meta(); ?>
+
     <title><?= $title ?: 'Title' ?></title>
 
 <!--    <link rel="apple-touch-icon" sizes="180x180" href="#">-->
