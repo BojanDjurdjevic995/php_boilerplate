03c5a116a15092ad3d13bfc3fbc445286000c461
bojan.djurdjevic <djurdjevicbojan12@gmail.com>
2020-09-15 11:38:53 +0200

    Fixed samo js functions, css with font awesome, delete some composer package

--- ajax/test.php
+++ ajax/test.php
@@ -3,6 +3,5 @@ require_once '../config/config.php';
 $r = request();
 if (request()->ajax() && $r->csrf_token == csrf_token())
 {
-    dd($r->all(), explode(',', $r->mustHave));
     return responseJSON($r->all());
 }
