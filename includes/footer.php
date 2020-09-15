b41216cdb45d9b865390d80d25fa5a6a2b8a205a
bojan.djurdjevic <djurdjevicbojan12@gmail.com>
2020-09-15 12:05:55 +0200

    Fixed bug in composer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <?php
 $page = explode('/', $_SERVER['SCRIPT_NAME']);
-$page = end($page); ?>
+$page = explode('.', end($page))[0]; ?>
 <script src="<?= asset('js/all-scripts.min.js') ?>"></script>
 <script src="<?= asset('js/main.js') ?>"></script>
 </body>
